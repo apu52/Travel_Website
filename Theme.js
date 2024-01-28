57e9701f5f28239240d54e2a98a5a01eb60c490c
AftabMankapure <aftabmankapure@gmail.com>
2024-01-29 00:34:37 +0530

    nav

--- Theme.js
+++ Theme.js
@@ -1,13 +1,27 @@
-const themebtn = document.getElementById("toggle");
+// const themebtn = document.getElementById("toggle");
 
-themebtn.onclick = () => {
+// themebtn.onclick = () => {
+//   document.body.classList.toggle("dark-theme");
+
+//   if (document.body.classList.contains("dark-theme")) {
+//     themebtn.innerText = "Light Theme";
+//     console.log("Dark theme");
+//   } else {
+//     themebtn.innerText = "Dark Theme";
+//     console.log("Light theme");
+//   }
+// };
+const themeToggle = document.getElementById("themeToggle");
+const themeLabel = document.querySelector(".toggle-label");
+
+themeToggle.addEventListener("change", () => {
   document.body.classList.toggle("dark-theme");
 
   if (document.body.classList.contains("dark-theme")) {
-    themebtn.innerText = "Light Theme";
+    themeLabel.style.background = "#fff"; // Adjust color for the checked state as needed
     console.log("Dark theme");
   } else {
-    themebtn.innerText = "Dark Theme";
+    themeLabel.style.background = "var(--primary-color)"; // Adjust color for the unchecked state as needed
     console.log("Light theme");
   }
-};
+});
