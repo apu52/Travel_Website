9b97211762efc1f24003706a5932ad784a92d7ea
Surajit Maity <sibu23122003@gmail.com>
2024-02-16 01:42:30 +0530

    Fixxed all Links & Added destinations pages

---
+++ dedicated destinations/Maldives/main.js
@@ -0,0 +1,22 @@
+// 'js/mian.js'
+
+let slider_img = document.querySelector('.slider-img');
+let images = ['maldive1.png', 'maldive2.png', 'maldive3.png', 'maldive4.png'];
+let i = 0;
+
+function prev(){
+	if(i <= 0) i = images.length;	
+	i--;
+	return setImg();			 
+}
+
+function next(){
+	if(i >= images.length-1) i = -1;
+	i++;
+	return setImg();			 
+}
+
+function setImg(){
+	return slider_img.setAttribute('src', "./gallery/"+images[i]);
+	
+}
