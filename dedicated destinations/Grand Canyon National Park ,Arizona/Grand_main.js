df3c2ebdddc7bbab46337ee6beef7751971d5504
maadi7 <mishraadi733@gmail.com>
2024-02-19 02:21:32 +0530

    climate info

--- dedicated destinations/Grand Canyon National Park ,Arizona/Grand_main.js
+++ dedicated destinations/Grand Canyon National Park ,Arizona/Grand_main.js
@@ -20,3 +20,33 @@ function setImg(){
 	return slider_img.setAttribute('src', "./gallery_Grand/"+images[i]);
 	
 }
+
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Grand%20Canyon&state=Arizona&country=US';
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '86715c3ce3msh290fe3d7d871fd4p122649jsn2309c0ae43db',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
+};
+
+// Fetch weather information and update HTML
+fetch(url, options)
+    .then(res => res.json())
+    .then(data => {
+		console.log(data)
+        const weatherInfoContainer = document.getElementById('weatherInfo');
+        
+        const humidity = data.humidity;
+		const max_temp = data.max_temp;
+		const min_temp = data.min_temp;
+
+        const weatherInfoHTML = `
+            <p>Maximum Temperature: ${max_temp}&deg;C</p>
+            <p>Minium Temperature: ${min_temp}&deg;C</p>
+            <p>Humidity: ${humidity}%</p>
+        `;
+        
+        weatherInfoContainer.innerHTML = weatherInfoHTML;
+    })
+    .catch(error => console.error('Error fetching weather data:', error));
