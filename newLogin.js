b58674893cbb514fcac9185e637fc37ec698a44f
IpsitaPrusty <ipstaprusty08@gmail.com>
2024-05-26 08:53:18 +0530

    fixed minor changes

---
+++ newLogin.js
@@ -0,0 +1,9 @@
+ // Function to show the pop-up
+ function showPopup() {
+    document.getElementById("loginPopup").style.display = "block";
+}
+
+// Function to close the pop-up
+function closePopup() {
+    document.getElementById("loginPopup").style.display = "none";
+}
