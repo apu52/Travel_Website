cc10338741199d69f8ef23cb815f15dbde031601
Yash Verma <114978813+Rajput-xv@users.noreply.github.com>
2024-07-14 20:08:57 +0530

    Revert "Merge branch 'main' into main"
    This reverts commit 46c5d98a3f68ad148fd2d7e276b1b5fba4a0d543, reversing
    changes made to 4437d4809ec526cd01c8c5913d30a9048decde85.

--- faq-website.js
+++ faq-website.js
@@ -1,3 +1,4 @@
+
 const accordions = document.querySelectorAll(".accordion");
 
 accordions.forEach((accordion, index) => {
@@ -18,68 +19,3 @@ accordions.forEach((accordion, index) => {
     });
   });
 });
-
-document.addEventListener("DOMContentLoaded", function () {
-  // Highlight active navbar
-  const navLinks = document.querySelectorAll(".navLinks a");
-
-  function highlightNavLink() {
-    const scrollPosition = window.scrollY;
-
-    navLinks.forEach((link) => {
-      const sectionId = link.getAttribute("href").substring(1);
-      const section = document.getElementById(sectionId);
-
-      // Adjust this value to control when the link should be highlighted
-      const offsetPercentage = 20;
-
-      if (
-        section.offsetTop - window.innerHeight * (offsetPercentage / 100) <=
-          scrollPosition &&
-        section.offsetTop + section.offsetHeight > scrollPosition
-      ) {
-        link.classList.add("active");
-      } else {
-        link.classList.remove("active");
-      }
-    });
-  }
-
-  document.addEventListener("scroll", highlightNavLink);
-  window.addEventListener("resize", highlightNavLink);
-
-  // Initialize AOS library
-  AOS.init();
-
-  // Set animation delays for FAQs
-  const faqs = document.querySelectorAll(".faq");
-  faqs.forEach((faq, index) => {
-    faq.style.animationDelay = `${index * 0.2}s`;
-  });
-});
-
-// Logout Button
-document.addEventListener("DOMContentLoaded", function () {
-  var isLoggedIn = localStorage.getItem("isLoggedIn");
-  var loginButton = document.getElementById("btn-style");
-  var logoutButton = document.getElementById("logout-btn");
-
-  if (isLoggedIn === "true") {
-    loginButton.style.display = "none";
-    logoutButton.style.display = "block";
-    loginButton.disabled = true;
-    logoutButton.disabled = false;
-    logoutButton.style.cursor = "pointer";
-    logoutButton.addEventListener("click", function () {
-      localStorage.setItem("isLoggedIn", "false");
-      window.location.href = "./index.html";
-    });
-  } else {
-    loginButton.style.display = "block";
-    logoutButton.disabled = true;
-  }
-});
-
-function sendEmail() {
-  window.location.href = "mailto:arpanchowdhury003@gmail.com";
-}
